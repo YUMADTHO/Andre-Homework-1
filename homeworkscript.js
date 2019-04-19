28f8e88f8cf8cee45a42766388a8e8b5c4fc0309
YUMADTHO <49726580+YUMADTHO@users.noreply.github.com>
2019-04-19 18:29:54 -0400

    Add files via upload

---
+++ homeworkscript.js
@@ -0,0 +1,21 @@
+function nicknameButton(){
+    var newText = document.getElementById('nickname').value;
+    document.getElementById('andrenickname').innerHTML = newText;
+
+}
+
+function andreface(){
+    document.getElementById('andre').src="andreface.jpg"
+}
+
+function andrehat(){
+    document.getElementById('andre').src="andrehat.jpg"
+}
+
+function andrebiceps(){
+    document.getElementById('andre').src="andrebiceps.jpg"
+}
+
+function andrefood(){
+    document.getElementById('andre').src="andrefood.jpg"
+}
